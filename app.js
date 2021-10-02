859a2142729ccc22188c0ce28095091040f4b38b
Dræmër <43190981+jssica824@users.noreply.github.com>
2021-10-02 15:22:42 +0800

    Updated

--- app.js
+++ app.js
@@ -7,32 +7,33 @@ let attempt = 1;
 let maxAttempt = 5;
 let history = "";
 
-
-button.addEventListener('click', function(){
+button.addEventListener('click', function () {
     let inputNum = document.getElementById("inputNum").value;
-    if (attempt < maxAttempt) 
-    {
-	    	if( inputNum == randomNumber) {
-	        alert("You Win!");
-		    location.reload();
-		    } 
-		    else if(inputNum < randomNumber) {
-		        attempt++;
-		        history = inputNum;
-		        $("ol").append("<li>" + history + " - " + "Guess Higher </li>");
-		        document.getElementById("inputNum").value = "";
-		    } 
-		    else if(inputNum > randomNumber) {
-		        attempt++;
-		        history = inputNum;
-		        $("ol").append("<li>" + history + " - " + "Guess Lower </li>");
-		        document.getElementById("inputNum").value = "";
-	    }
+    //If attempt is lower than maxAttempt
+    //check whether input number and random number are the same
+    if (attempt < maxAttempt) {
+        if (inputNum === randomNumber) {
+            alert("You Win!");
+            location.reload();
+        } else if (inputNum < randomNumber) {
+            attempt++;
+            history = inputNum;
+            $("ul").append("<li>" + history + " - " + "Guess Higher </li>");
+            document.getElementById("inputNum").value = "";
+        } else if (inputNum > randomNumber) {
+            attempt++;
+            history = inputNum;
+            $("ul").append("<li>" + history + " - " + "Guess Lower </li>");
+            document.getElementById("inputNum").value = "";
+        }
     } else {
-    	history = inputNum;
-		$("ol").append("<li>" + history + " - " + "Guess Lower </li>");
-    	alert("Game Over!");
-    	location.reload();
+        history = inputNum;
+        $("ul").append("<li>" + history + " - " + "Guess Lower </li>");
+        setTimeout(function () {
+            alert("Game Over!");
+        }, 500);
+        setTimeout(function () {
+            location.reload()
+        }, 1000);
     }
-    
 })
